3e47c0783b1b5648ce24a355304697cc88d7d51b
fancyfreeman <chenxin0524@qq.com>
2018-05-25 23:13:22 +0800

    Update cdeploy.py

--- cdeploy.py
+++ cdeploy.py
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-from dev
+#nbgrader
+#处理course list
+cp ./nbgrader/source/handlers.py /opt/conda/lib/python3.6/site-packages/nbgrader/server_extensions/assignment_list/handlers.py
+echo "End"
