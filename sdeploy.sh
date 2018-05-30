b84d060374d9e2bc2370eb2022af7a04d62e084c
fancyfreeman <chenxin0524@qq.com>
2018-05-31 01:37:21 +0800

    new

--- sdeploy.sh
+++ sdeploy.sh
@@ -7,7 +7,8 @@ cp ./jupyterhub/config/*.* /srv/jupyterhub
 #wordpress
 cp ./wordpress/config/wp-config.php         /home/wwwroot/default/wp-config.php
 cp ./wordpress/config/default-constants.php /home/wwwroot/default/wp-includes/default-constants.php
-
+cp ./wordpress/learnPress/zh_CN.mo          /home/wwwroot/default/wp-content/plugins/learnpress/languages/learnpress-zh_CN.mo
+cp ./wordpress/learnPress/zh_CN.po          /home/wwwroot/default/wp-content/plugins/learnpress/languages/learnpress-zh_CN.po
 
 #php
 cp ./php/config/php.ini      /usr/local/php/etc/php.ini
